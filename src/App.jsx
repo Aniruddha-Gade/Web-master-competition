b0bb0c9e7d30e750ea61a51d510d9b3c2a94f59f
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-10-15 13:50:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+
+
+  return (
+<div className=''>
+      <h1 className='text-7xl text-red-600 font-bold'>
+Aniruddha Gade
+      </h1>
+
+    </div>
+  )
+}
+
+export default App
